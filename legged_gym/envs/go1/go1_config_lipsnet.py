297dfacd5e9a3c73caa5c23c3d7d5bc14e097b7e
LucienJi <984592975@qq.com>
2023-10-20 18:31:22 +0800

    before merge

---
+++ legged_gym/envs/go1/go1_config_lipsnet.py
@@ -0,0 +1,217 @@
+# SPDX-FileCopyrightText: Copyright (c) 2021 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Copyright (c) 2021 ETH Zurich, Nikita Rudin
+
+from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
+
+class LipsGo1RoughCfg( LeggedRobotCfg ):
+    class init_state( LeggedRobotCfg.init_state ):
+        pos = [0.0, 0.0, 0.42] # x,y,z [m]
+        default_joint_angles = { # = target angles [rad] when action = 0.0
+            'FL_hip_joint': 0.1,   # [rad]
+            'RL_hip_joint': 0.1,   # [rad]
+            'FR_hip_joint': -0.1 ,  # [rad]
+            'RR_hip_joint': -0.1,   # [rad]
+
+            'FL_thigh_joint': 0.8,     # [rad]
+            'RL_thigh_joint': 1.,   # [rad]
+            'FR_thigh_joint': 0.8,     # [rad]
+            'RR_thigh_joint': 1.,   # [rad]
+
+            'FL_calf_joint': -1.5,   # [rad]
+            'RL_calf_joint': -1.5,    # [rad]
+            'FR_calf_joint': -1.5,  # [rad]
+            'RR_calf_joint': -1.5,    # [rad]
+        }
+
+    class env:
+        num_envs = 4096
+        num_observations = 235 - 11 * 17 
+        num_privileged_obs = None # if not None a priviledge_obs_buf will be returned by step() (critic obs for assymetric training). None is returned otherwise 
+        num_actions = 12
+        env_spacing = 3.  # not used with heightfields/trimeshes 
+        send_timeouts = True # send time out information to the algorithm
+        episode_length_s = 20 # episode length in seconds
+
+    class terrain:
+        mesh_type = 'plane' # "heightfield" # none, plane, heightfield or trimesh
+        horizontal_scale = 0.1 # [m]
+        vertical_scale = 0.005 # [m]
+        border_size = 25 # [m]
+        curriculum = True
+        static_friction = 1.0
+        dynamic_friction = 1.0
+        restitution = 0.
+        # rough terrain only:
+        measure_heights = False
+        measured_points_x = [-0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8] # 1mx1.6m rectangle (without center line)
+        measured_points_y = [-0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5]
+        
+        # selected = False # select a unique terrain type and pass all arguments
+        # terrain_kwargs = None # Dict of arguments for selected terrain
+
+        # selected = True 
+        # terrain_kwargs = {
+        #     "type":"random_uniform_terrain",
+            
+        #     "min_height" : -0.03, 
+        #     "max_height" : 0.03, 
+        #     "step": 0.005, 
+        #     "downsampled_scale":0.2,
+            
+            
+        # }
+
+        
+        
+        max_init_terrain_level = 5 # starting curriculum state
+        terrain_length = 8.
+        terrain_width = 8.
+        num_rows= 10 # number of terrain rows (levels)
+        num_cols = 20 # number of terrain cols (types)
+        # terrain types: [smooth slope, rough slope, stairs up, stairs down, discrete]
+        terrain_proportions = [0, 1, 0, 0, 0, 0, 0, 0]  # caozhanxiang提供的地形
+        # terrain_proportions = [0.1, 0.1, 0.35, 0.25, 0.2]
+        # trimesh only:
+        slope_treshold = 0.75 # slopes above this threshold will be corrected to vertical surfaces
+
+    class control( LeggedRobotCfg.control ):
+        # PD Drive parameters:
+        control_type = 'P'
+        stiffness = {'joint': 20.}  # [N*m/rad]
+        damping = {'joint': 0.5}     # [N*m*s/rad]
+        # action scale: target angle = actionScale * action + defaultAngle
+        action_scale = 0.25
+        # decimation: Number of control action updates @ sim DT per policy DT
+        decimation = 4
+
+    class asset( LeggedRobotCfg.asset ):
+        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/go1/urdf/go1_wtw.urdf'
+        name = "go1"
+        foot_name = "foot"
+        penalize_contacts_on = ["thigh", "calf"]
+        terminate_after_contacts_on = ["base"]
+        self_collisions = 1 # 1 to disable, 0 to enable...bitwise filter
+  
+    class rewards( LeggedRobotCfg.rewards ):
+        soft_dof_pos_limit = 0.9
+        base_height_target = 0.25
+        
+        only_positive_rewards = True # if true negative total rewards are clipped at zero (avoids early termination problems)
+        tracking_sigma = 0.25 # tracking reward = exp(-error^2/sigma)
+        soft_dof_vel_limit = 1.
+        soft_torque_limit = 1.
+        max_contact_force = 100. # forces above this value are penalized
+        class scales( LeggedRobotCfg.rewards.scales ):
+            # torques = -0.0002
+            torques = -0.0002  # -0.0002
+            dof_pos_limits = -10.0
+            
+            termination = -0.0
+            tracking_lin_vel = 1.0
+            tracking_ang_vel = 0.5
+            lin_vel_z = -2.0 # -2.0
+            ang_vel_xy = -0.05 # -0.05
+            orientation = -0.
+            dof_vel = -0.
+            dof_acc = -5e-7 # -2.5e-7
+            base_height = -0. 
+            feet_air_time = 1.0 # 1.0
+            collision = -1. # -1.0
+            feet_stumble = -0.0 
+            action_rate = -0.0 # -0.01
+            stand_still = -0.
+    
+    class commands:
+        curriculum = False
+        max_curriculum = 3.
+        num_commands = 4 # default: lin_vel_x, lin_vel_y, ang_vel_yaw, heading (in heading mode ang_vel_yaw is recomputed from heading error)
+        resampling_time = 10. # time before command are changed[s]
+        heading_command = False # if true: compute ang vel command from heading error
+        class ranges:
+            lin_vel_x = [-0.5, 1.] # min max [m/s]
+            lin_vel_y = [-0.3, 0.3]   # min max [m/s]
+            ang_vel_yaw = [-1, 1]    # min max [rad/s]
+            heading = [-3.14, 3.14]
+            
+    class domain_rand:
+        randomize_friction = False
+        friction_range = [0.5, 1.25]
+        randomize_base_mass = False
+        added_mass_range = [-2., 2.]
+        push_robots = False
+        push_interval_s = 15
+        max_push_vel_xy = 1.
+
+class LipsGo1RoughCfgPPO( LeggedRobotCfgPPO ):
+    class policy:  # policy神经网络设置
+        init_noise_std = 1.0
+        actor_hidden_dims = [512, 256, 128]
+        critic_hidden_dims = [512, 256, 128]
+        activation = 'lrelu' # can be elu, relu, selu, crelu, lrelu, tanh, sigmoid
+        k_hidden_dims = [256, 256, 256]
+        k_init = 1
+        k_max = 500
+        loss_lambda = 1e-5
+        k_type = 0 # 0: no k, 1: scalar, 2: vector
+        jacob_type = 1 # 1: dim = (1,2), 2: dim = (-1)
+
+    class algorithm( LeggedRobotCfgPPO.algorithm ):
+        value_loss_coef = 1.0
+        use_clipped_value_loss = True
+        clip_param = 0.2
+        entropy_coef = 0.01
+        num_learning_epochs = 5
+        num_mini_batches = 4 # mini batch size = num_envs*nsteps / nminibatches
+        learning_rate = 1e-3 #5.e-4
+        k_learning_rate = 5.e-6
+        schedule = 'adaptive' # could be adaptive, fixed
+        gamma = 0.99
+        lam = 0.95
+        desired_kl = 0.01
+        max_grad_norm = 1.
+
+    class runner( LeggedRobotCfgPPO.runner ):
+        
+        num_steps_per_env = 24 # per iteration
+        max_iterations = 5000 # number of policy updates
+        # logging
+        save_interval = 100 # check for potential saves every this many iterations
+        
+        run_name = 'Stage3_k0_no_action_reward'
+        experiment_name = 'go1_lipsnet'
+        stage = 3
+
+        # load and resume
+        resume = False
+        load_run = -1 # -1 = last run
+        checkpoint = -1 # -1 = last saved model
+        resume_path = None # updated from load_run and chkpt
+
+  
---
+++ rsl_rl/modules/lipsnet.py
@@ -0,0 +1,204 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from functorch import jacrev, vmap
+
+
+def mlp(sizes, hid_nonliear, out_nonliear):
+    # declare layers
+    layers = []
+    for j in range(len(sizes) - 1):
+        nonliear = hid_nonliear if j < len(sizes) - 2 else out_nonliear
+        layers += [nn.Linear(sizes[j], sizes[j + 1]), nonliear]
+    # init weight
+    for i in range(len(layers) - 1):
+        if isinstance(layers[i], nn.Linear):
+            if isinstance(layers[i+1], nn.ReLU):
+                nn.init.kaiming_normal_(layers[i].weight, nonlinearity='relu')
+            elif isinstance(layers[i+1], nn.LeakyReLU):
+                nn.init.kaiming_normal_(layers[i].weight, nonlinearity='leaky_relu')
+            else:
+                nn.init.xavier_normal_(layers[i].weight)
+    return nn.Sequential(*layers)
+
+
+class K_net(nn.Module):
+    def __init__(self, global_lips, k_init, sizes, hid_nonliear, out_nonliear) -> None:
+        super().__init__()
+        self.global_lips = global_lips
+        if global_lips:
+            # declare global Lipschitz constant
+            self.k = torch.nn.Parameter(torch.tensor(k_init, dtype=torch.float), requires_grad=True)
+        else:
+            # declare network
+            self.k = mlp(sizes, hid_nonliear, out_nonliear)
+            # set K_init
+            self.k[-2].bias.data += torch.tensor(k_init, dtype=torch.float).data
+
+    def forward(self, x):
+        if self.global_lips:
+            return F.softplus(self.k).repeat(x.shape[0]).unsqueeze(1)
+        else:
+            return self.k(x)
+        
+
+class LipsNet(nn.Module):
+    def __init__(self, f_sizes,k_sizes, global_lips=True, k_init=1,
+                 f_hid_nonliear=nn.ReLU, f_out_nonliear=nn.Identity,
+                k_hid_act=nn.Tanh, k_out_act=nn.Identity,k_type = 1,
+                 loss_lambda=0.1, eps=1e-4, squash_action=False) -> None:
+        super().__init__()
+        print("################### LipsNet Test ###################")
+        # declare network
+        self.k_type = k_type 
+        self.f_net = mlp(f_sizes, f_hid_nonliear, f_out_nonliear)
+        self.k_net = K_net(global_lips, k_init, k_sizes, k_hid_act, k_out_act)
+        # declare hyperparameters
+        self.loss_lambda = loss_lambda
+        self.eps = eps
+        self.squash_action = squash_action
+        # initialize as eval mode
+        self.eval()
+
+    def forward(self, x):
+        # K(x) forward
+        k_out = self.k_net(x)
+        # L2 regularization backward
+        if self.training and k_out.requires_grad:
+            lips_loss = self.loss_lambda * (k_out ** 2).mean()
+            lips_loss.backward(retain_graph=True)
+        # f(x) forward
+        f_out = self.f_net(x)
+        # calcute jac matrix
+        if k_out.requires_grad:
+            jacobi = vmap(jacrev(self.f_net))(x)
+        else:
+            with torch.no_grad():
+                jacobi = vmap(jacrev(self.f_net))(x)
+        # jacobi.dim: (x.shape[0], f_out.shape[1], x.shape[1])
+        #             (batch     , f output dim  , x feature dim)
+        # calcute jac norm
+        jac_norm = torch.norm(jacobi, 2, dim=(1,2)).unsqueeze(1)
+        
+        # multi-dimensional gradient normalization (MGN)
+        if self.k_type == 0:
+            action = f_out / (jac_norm + self.eps)
+        else:
+            action = k_out * f_out / (jac_norm + self.eps)
+        # squash action
+        if self.squash_action:
+            action = torch.tanh(action)
+        return action
+    
+
+
+
+
+
+class ExpLipsNet(nn.Module):
+    def __init__(self, f_sizes,k_sizes, global_lips, k_init,k_max,
+                 f_hid_nonliear, f_out_nonliear,
+                k_hid_act, k_out_act,k_type = 1,jacob_type = 1,
+                 loss_lambda=0.1, eps=1e-4, squash_action=False) -> None:
+        super().__init__()
+        print("################### LipsNet Test ###################")
+        # declare network
+        self.k_type = k_type 
+        self.jacob_type = jacob_type 
+        self.f_net = mlp(f_sizes, f_hid_nonliear, f_out_nonliear)
+        self.k_net = K_net(global_lips, k_init, k_sizes, k_hid_act, k_out_act)
+        # declare hyperparameters
+        self.loss_lambda = loss_lambda
+        self.eps = eps
+        self.k_max = k_max 
+        self.squash_action = squash_action
+        # initialize as eval mode
+        self.eval()
+    
+    def check_jacob(self,x):
+        with torch.no_grad():
+            jacobi = vmap(jacrev(self.f_net))(x)
+        jac_norm = torch.norm(jacobi, 2, dim=(1,2)).unsqueeze(1)
+        return jac_norm
+
+    def lips_forward(self, x):
+        # K(x) forward
+        k_out = self.k_net(x)
+        
+        # f(x) forward
+        f_out = self.f_net(x)
+        # calcute jac matrix
+        if k_out.requires_grad:
+            jacobi = vmap(jacrev(self.f_net))(x)
+        else:
+            with torch.no_grad():
+                jacobi = vmap(jacrev(self.f_net))(x)
+        # jacobi.dim: (x.shape[0], f_out.shape[1], x.shape[1])
+        #             (batch     , f output dim  , x feature dim)
+        # calcute jac norm
+        if self.jacob_type == 1:
+            jac_norm = torch.norm(jacobi, 2, dim=(1,2)).unsqueeze(1)
+        else:
+            jac_norm = torch.norm(jacobi, 2, dim=-1)
+        # multi-dimensional gradient normalization (MGN)
+
+        if self.k_type == 0:
+            action = f_out / (jac_norm + self.eps)
+        else:
+            action = k_out * f_out / (jac_norm + self.eps)
+
+        # squash action
+        if self.squash_action:
+            action = torch.tanh(action)
+        return action
+    
+    def raw_forward(self, x):
+        action = self.f_net(x)
+        if self.squash_action:
+            action = torch.tanh(action)
+        return action
+    
+    def raw_jacob_check(self,x,ord = 2):
+        with torch.no_grad():
+            jacobi = vmap(jacrev(self.f_net))(x)
+        jac_norm = torch.norm(jacobi, ord, dim=(1,2))
+        info = {
+            'max_jacob':jac_norm.max().item(),
+            'min_jacob':jac_norm.min().item(),
+            'mean_jacob':jac_norm.mean().item(),
+        }
+        return info
+
+    def sl_k_loss(self,x):
+        k_out = self.k_net(x)
+        jacob = self.check_jacob(x)
+        sl_loss = (k_out - jacob)**2 
+        penality = torch.clip( k_out - self.k_max, min = 0.0)
+        lips_loss = sl_loss + penality
+        lips_loss = lips_loss.mean()
+        loss = {
+            'sl_loss':sl_loss.mean().item(),
+            'penality':penality.mean().item(),
+            'loss': lips_loss,
+            'max_jacob':jacob.max().item(),
+            'min_jacob':jacob.min().item(),
+            'mean_jacob':jacob.mean().item(),
+        }
+        return loss 
+
+    def l2_regularization(self,x):
+        # L2 regularization backward
+        k_out = self.k_net(x)   
+        jacob = self.check_jacob(x)
+        if self.training and k_out.requires_grad:
+            l2_loss = (k_out ** 2).mean()
+            penality = torch.clip( k_out - self.k_max, min = 0.0).mean()
+        loss = {
+            'l2_loss':l2_loss.item(),
+            'penality':penality.item(),
+            'loss': l2_loss * self.loss_lambda + penality,
+            'max_jacob':jacob.max().item(),
+            'min_jacob':jacob.min().item(),
+            'mean_jacob':jacob.mean().item(),
+        }
+        return loss
